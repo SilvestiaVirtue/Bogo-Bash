7bb5fe9187c20f3cb98caf68af5627cf666dc755
SilvestiaVirtue <shkinyoo@gmail.com>
2025-12-26 23:07:26 +0400

    Create crdir_fa.c

---
+++ src/commands/command_with_arguments/crdir_fa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+void cmd_crdir_fa(int argc, char **argv){
+    if(argc == 3){
+        int rights = 0777;
+        int result =  mkdir(argv[2], rights);
+        if(result == 0){
+            printf("Bogo Bash: Done\n");
+        }else{
+            printf("Bogo Bash:  Error creating directory '%s' \n", argv[2]);
+        }
+        }else{
+            printf("Bogo Bash:  Нечего создавать (обратитесь к справочнику 'help')\n");
+    }
+}
